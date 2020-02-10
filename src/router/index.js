180af80b5d4e44a6af7d980e1007d774f93a0d28
agnesnakalembe <nakalembeagnes8@gmail.com>
2020-02-10 16:52:44 +0300

    Creates landing route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Signup from '../views/Signup';
+import Home from '../views/Home';
 
 Vue.use(Router)
 
 export default new Router({
     routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
         {
             path: '/signup',
             name: 'signup',
