0da5cccb0be1c542f43eadd84127c89ad3840efd
agnesnakalembe <nakalembeagnes8@gmail.com>
2020-02-10 16:53:03 +0300

    Creates home view

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-link class="btn btn-primary" to="/signup">signup</router-link>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+    name: 'home'
+}
+</script>
+
